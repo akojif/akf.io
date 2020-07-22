faca218fcf7fca079726a91036f971bd3f7698b0
Akoji Francis <jidwithfa007@crmintcol.org>
2020-07-22 19:26:06 +0100

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  title: `Francis's Blog`,
-  description: `Blog posted about ...`,
-  author: `[Akoji Francis]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://akf-blog.vercel.app`, // Your blog site url
-  social: {
-    twitter: `akoji_f`, // Your Twitter account
-    github: `akojif`, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: `akojif`, // Your LinkedIn account
-  },
-  icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
-  comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'akojif/akf-blog', // Your repository for archive comment
-  },
-  configs: {
-    countOfInitialPost: 10, // Config your initial count of post
-  },
-  sponsor: {
-    buyMeACoffeeId: 'akojif',
-  },
-  share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
-  },
-  ga: '', // Add your google analytics tranking ID
+    title: `Akf's Blog`,
+    description: `Blog posted about ...`,
+    author: `Akf`,
+    introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+    siteUrl: `https://akf-blog.vercel.app`, // Your blog site url
+    social: {
+        twitter: `akoji_f`, // Your Twitter account
+        github: `akojif`, // Your GitHub account
+        medium: ``, // Your Medium account
+        facebook: ``, // Your Facebook account
+        linkedin: `akojif`, // Your LinkedIn account
+    },
+    icon: `content/assets/felog.png`, // Add your favicon
+    keywords: [`blog`],
+    comment: {
+        disqusShortName: '', // Your disqus-short-name. check disqus.com.
+        utterances: 'akojif/akf-blog', // Your repository for archive comment
+    },
+    configs: {
+        countOfInitialPost: 10, // Config your initial count of post
+    },
+    sponsor: {
+        buyMeACoffeeId: 'akojif',
+    },
+    share: {
+        facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    },
+    ga: '', // Add your google analytics tranking ID
 }
